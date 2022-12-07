8d84a2307bbf64d9c3bdf5c6af12b21050e2c2e9
ahmed mohamed <ahmedmohamed660000@gmail.com>
2022-12-07 16:18:07 +0200

    preprocessind train-dataset-done

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,16 +1,81 @@
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+
 
 # Split car-info column
 def split(data):
     new = data["car-info"].str.split(",", n=2, expand=True)
-    data_train["Model"] = new[0]
-    data_train["company"] = new[1]
-    data_train["date"] = new[2]
+    data["Model"] = new[0]
+    data["company"] = new[1]
+    data["date"] = new[2]
 
-    data_train['date'] = data_train['date'].str.replace('\W', '', regex=True)
-    data_train['Model'] = data_train['Model'].str.replace('\W', '', regex=True)
-    data_train['company'] = data_train['company'].str.replace('\W', '', regex=True)
+    data['date'] = data['date'].str.replace('\W', '', regex=True)
+    data['Model'] = data['Model'].str.replace('\W', '', regex=True)
+    data['company'] = data['company'].str.replace('\W', '', regex=True)
 
-    data_train.insert(1, 'Model', data_train.pop('Model'))
-    data_train.insert(2, 'company', data_train.pop('company'))
-    data_train.insert(3, 'date', data_train.pop('date'))
-    data_train.drop(columns=["car-info"], inplace=True)
+    data.insert(1, 'Model', data.pop('Model'))
+    data.insert(2, 'company', data.pop('company'))
+    data.insert(3, 'date', data.pop('date'))
+    data.drop(columns=["car-info"], inplace=True)
+    return data
+
+
+# Change type of column ot datetime
+def datetime(data, col):
+    data[col] = pd.to_datetime(data[col])
+    return data
+
+
+# Convert  column to lowercase
+def LowerCase(data, col):
+    data[col] = data[col].apply(str.lower)
+    return data
+
+
+# Fill NAN num column with median
+def Fill_median(data, col):
+    data[col] = data[col].fillna(data[col].median())
+    return data
+
+
+# Fill NAN Categorical column with permutation
+def Fill_Cate(data, cols):
+    for col in cols:
+        # creates a random permutation of the categorical values
+        permutation = np.random.permutation(data[col])
+
+        # erase the empty values
+        empty = np.where(permutation == "")
+        permutation = np.delete(permutation, empty)
+
+        # replace all empty values of the dataframe[segment]
+        end = len(permutation)
+        data[col] = data[col].apply(lambda x: permutation[np.random.randint(end)] if pd.isnull(x) else x)
+        # data[col] = data[col].fillna(data[col].mode()[0])
+        return data
+
+
+# Applying label Encoding
+def Feature_Encoder(data, cols):
+    for c in cols:
+        lbl = LabelEncoder()
+        lbl.fit(list(data[c].values))
+        data[c] = lbl.transform(list(data[c].values))
+    return data
+
+
+# Correlation between columns
+def Corr(data, col):
+    corr = data.corr()
+    top_feature = corr.index[abs(corr[col]) > 0.1]
+    # Correlation plot
+    plt.subplots(figsize=(12, 8))
+    top_corr = data[top_feature].corr()
+    sns.heatmap(top_corr, annot=True)
+    plt.show()
+    top_feature = top_feature.delete(-1)
+    X = data[top_feature]
+    return X
--- model.py
+++ model.py
@@ -1,74 +1,45 @@
-import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import seaborn as sns
-from matplotlib import pyplot as plt
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+
+import PreProcessing as pre
 
 data_train = pd.read_csv('cars-train.csv')
 data_test = pd.read_csv('cars-test.csv')
 
-data_train['date'] = pd.to_datetime(data_train['date'])
+# Split car-info column
+data_train = pre.split(data_train)
 
-# Convert fuel_type column to lowercase
-data_train['fuel_type'] = data_train['fuel_type'].apply(str.lower)
+# Change type of column ot datetime
+pre.datetime(data_train, 'date')
 
-data_train['volume(cm3)'] = data_train['volume(cm3)'].fillna(data_train['volume(cm3)'].median())
+# Convert  column to lowercase
+pre.LowerCase(data_train, 'fuel_type')
+
+# Fill NAN volume(cm3) colum with median
+pre.Fill_median(data_train, 'volume(cm3)')
 
 data_train['segment'].isnull().value_counts().plot.bar()
-
 data_train['drive_unit'].isnull().value_counts().plot.bar()
 
 # Fill NAN values in [segment , drive_unit] columns
 cols = ['segment', 'drive_unit']
-for col in cols:
-    # creates a random permutation of the categorical values
-    permutation = np.random.permutation(data_train[col])
-
-    # erase the empty values
-    empty = np.where(permutation == "")
-    permutation = np.delete(permutation, empty)
-
-    # replace all empty values of the dataframe[segment]
-    end = len(permutation)
-    data_train[col] = data_train[col].apply(lambda x: permutation[np.random.randint(end)] if pd.isnull(x) else x)
-    data_train[col] = data_train[col].fillna(data_train[col].mode()[0])
+pre.Fill_Cate(data_train, cols)
 # Check outliers
 # for col in data_train.columns:
 #     fig = px.histogram(data_train, x=col)
 #     fig.show()
-
-cols = list(data_train.select_dtypes('object'))
-
-
 # Applying label Encoding
+Cate_cols = list(data_train.select_dtypes('object'))
 
-def Feature_Encoder(data, cols):
-    for c in cols:
-        lbl = LabelEncoder()
-        lbl.fit(list(data[c].values))
-        data[c] = lbl.transform(list(data[c].values))
-    return data
+data_train = pre.Feature_Encoder(data_train, Cate_cols)
+X = pre.Corr(data_train, 'Price Category')
 
-
-data_train = Feature_Encoder(data_train, cols)
-
-# Correlation between columns
-corr = data_train.corr()
-# Top 50% Correlation training features with the Value
-top_feature = corr.index[abs(corr['Price Category']) > 0.1]
-# Correlation plot
-plt.subplots(figsize=(12, 8))
-top_corr = data_train[top_feature].corr()
-sns.heatmap(top_corr, annot=True)
-plt.show()
-top_feature = top_feature.delete(-1)
-X = data_train[top_feature]
 
 X  # Selected Features
 Y = data_train['Price Category']  # Target
+
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
