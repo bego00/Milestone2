403aec9bf02334c74a4a25699ae09bd3465056d2
ahmed mohamed <ahmedmohamed660000@gmail.com>
2022-12-07 16:59:36 +0200

    preprocessing done at all dataset

--- model.py
+++ model.py
@@ -1,16 +1,15 @@
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-from sklearn import metrics
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 
 import PreProcessing as pre
 
 data_train = pd.read_csv('cars-train.csv')
 data_test = pd.read_csv('cars-test.csv')
 
+####################### Train data-Set ###################################
+
 # Split car-info column
 data_train = pre.split(data_train)
-
+data_train.drop(['car_id'], axis=1)
 # Change type of column ot datetime
 pre.datetime(data_train, 'date')
 
@@ -34,20 +33,48 @@ pre.Fill_Cate(data_train, cols)
 Cate_cols = list(data_train.select_dtypes('object'))
 
 data_train = pre.Feature_Encoder(data_train, Cate_cols)
-X = pre.Corr(data_train, 'Price Category')
+
+# X = pre.Corr(data_train, 'Price Category')
+#
+#
+# X  # Selected Features
+# Y = data_train['Price Category']  # Target
+#
+# # Split dataset into training set and test set
+# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+#
+# # Create a Gaussian Classifier
+# clf = RandomForestClassifier(n_estimators=100, max_depth=15)
+#
+# # Train the model using the training sets y_pred=clf.predict(X_test)
+# clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+# # Model Accuracy, how often is the classifier correct?
+# print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 
-X  # Selected Features
-Y = data_train['Price Category']  # Target
+####################### Test data-Set ###################################
 
-# Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+# Split car-info column
+data_tes = pre.split(data_test)
 
-# Create a Gaussian Classifier
-clf = RandomForestClassifier(n_estimators=100, max_depth=15)
+# Change type of column ot datetime
+pre.datetime(data_test, 'date')
 
-# Train the model using the training sets y_pred=clf.predict(X_test)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-# Model Accuracy, how often is the classifier correct?
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+# Convert  column to lowercase
+pre.LowerCase(data_test, 'fuel_type')
+
+# Fill NAN volume(cm3) colum with median
+pre.Fill_median(data_test, 'volume(cm3)')
+
+# Fill NAN values in [segment , drive_unit] columns
+cols = ['segment', 'drive_unit']
+pre.Fill_Cate(data_test, cols)
+# Check outliers
+# for col in data_test.columns:
+#     fig = px.histogram(data_test, x=col)
+#     fig.show()
+# Applying label Encoding
+Cate_cols = list(data_test.select_dtypes('object'))
+
+data_test = pre.Feature_Encoder(data_test, Cate_cols)
